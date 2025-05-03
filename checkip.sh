27f8e65f154f947d25d06091173440835db3443e
Emalux <108554070+Emalux@users.noreply.github.com>
2025-05-03 10:23:39 -0300

    Update checkip.sh

--- checkip.sh
+++ checkip.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Definir opções
-opcoes=("icanhazip.com" "http://checkip.amazonaws.com" "http://ipecho.net/plain" "l2.io/ip" "ifconfig.me/ip")
+options=("icanhazip.com" "http://checkip.amazonaws.com" "http://ipecho.net/plain" "l2.io/ip" "ifconfig.me/ip")
 
 # Exibir o menu e obter a escolha do usuário
-select opcao in "${opcoes[@]}"; do
-    echo "Você escolheu: ${opcao}"
+select options in "${options[@]}"; do
+    echo "you chose: ${options}"
     break # Saír do loop depois de uma escolha
 done
 
 # Executar ações com base na escolha
-case "$opcao" in
+case "$options" in
     "icanhazip.com")
         curl icanhazip.com
         ;;
@@ -27,6 +27,6 @@ case "$opcao" in
         curl ifconfig.me/ip
         ;;
     *)
-        echo "Opção inválida"
+        echo "Invalid option"
         ;;
 esac
